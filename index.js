9edfbdf7ff46cda02eeb0a168a10eb263a5d91b2
IamWilmar <wilmardaniel25@gmail.com>
2021-02-12 23:41:39 -0500

    Autenticacion login y JWT

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
+
+//DB config
+const { dbConnection } = require('./database/config');
+dbConnection();
+
 //App de express
 const app = express();
+
+//Lectura y parse del body
+app.use(express.json());
 
 //Node server
 const server = require('http').createServer(app);
@@ -13,6 +21,10 @@ require('./sockets/socket');
 //Path public
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
+
+
+// mis Rutas
+app.use('/api/login', require('./routes/auth'));
 
 
 server.listen(process.env.PORT, (err) => {
